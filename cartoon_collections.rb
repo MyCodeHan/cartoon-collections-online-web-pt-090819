291d3b305038bda269a68fcf8a372ea55ce4a506
Hannah Harrington <hannahharrington@outlook.com>
2019-09-26 02:53:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)
-  puts ["Grumpy", "Dopey", "Bashful", "Sleepy"].each
+  puts ["Grumpy", "Dopey", "Bashful", "Sleepy"].index
 end
 
 def summon_captain_planet# code an argument here
